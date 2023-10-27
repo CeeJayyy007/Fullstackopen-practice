a6818e23889750b1e6b033ba5403aa8e25076a66
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-27 02:19:57 -0400

    add cors to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Allow requests from any origin
+app.use(cors());
 
 app.use(express.json());
 
@@ -84,6 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
