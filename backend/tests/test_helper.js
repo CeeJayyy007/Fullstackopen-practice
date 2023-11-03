29db7057817af6d0d61e96fac744d36676164f60
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-03 16:06:44 -0400

    update test_helper

--- backend/tests/test_helper.js
+++ backend/tests/test_helper.js
@@ -1,13 +1,29 @@
 const Note = require("../models/note");
+const User = require("../models/user");
 
 const initialNotes = [
   {
     content: "HTML is easy",
     important: false,
+    userId: "5f8e3b2c1c2e6a2a4c3a1f2d",
   },
   {
     content: "Browser can execute only JavaScript",
     important: true,
+    userId: "5f8e3b2c1c2e6a2a4c3a1f2d",
+  },
+];
+
+const initialUsers = [
+  {
+    username: "root",
+    name: "Superuser",
+    password: "sekret",
+  },
+  {
+    username: "mluukkai",
+    name: "Matti Luukkainen",
+    password: "salainen",
   },
 ];
 
@@ -25,8 +41,16 @@ const notesInDb = async () => {
   return notes.map((note) => note.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+
+  return users.map((user) => user.toJSON());
+};
+
 module.exports = {
   initialNotes,
+  initialUsers,
   nonExistingId,
   notesInDb,
+  usersInDb,
 };
