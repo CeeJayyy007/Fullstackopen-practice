d7a6bbb35efc88ee810835db1f7ca18c771a0bd5
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-03 16:08:57 -0400

    update notes controller

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -4,9 +4,12 @@ const notesRouter = require("express").Router();
 // add note model
 const Note = require("../models/note");
 
+//add user model
+const User = require("../models/user");
+
 // get notes
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -37,12 +40,17 @@ notesRouter.put("/:id", async (request, response) => {
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
   response.status(201).json(savedNote);
 });
 
