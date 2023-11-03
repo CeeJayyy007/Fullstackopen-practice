a4b81fc78ceb9d118fe47fbda19de2f6dea90c42
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-03 03:17:07 -0400

    remove try and catch from notes controller

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -11,31 +11,26 @@ notesRouter.get("/", async (request, response) => {
 });
 
 // get notes with id
-notesRouter.get("/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.get("/:id", async (request, response) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
 });
 
 // toggle note importance
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", async (request, response) => {
   const { content, important } = request.body;
 
-  Note.findByIdAndUpdate(
+  const updatedNote = await Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
     { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
+  );
+
+  response.json(updatedNote);
 });
 
 // post note
@@ -48,18 +43,13 @@ notesRouter.post("/", async (request, response) => {
   });
 
   const savedNote = await note.save();
-
   response.status(201).json(savedNote);
 });
 
 // delete note
-notesRouter.delete("/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    // eslint-disable-next-line no-unused-vars
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = notesRouter;
