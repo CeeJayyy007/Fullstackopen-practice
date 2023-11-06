61dc88afd60417cdde2303739d5e1be09841bb6f
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-06 06:03:23 -0500

    add form component

---
+++ notes/src/components/Forms.jsx
@@ -0,0 +1,40 @@
+//login form
+const loginForm = (
+  username,
+  password,
+  handleLogin,
+  setUsername,
+  setPassword
+) => (
+  <form onSubmit={handleLogin}>
+    <div>
+      Username
+      <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+    </div>
+    <div>
+      Password
+      <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+);
+
+// note form
+const noteForm = (addNote, newNote, handleNoteChange) => (
+  <form onSubmit={addNote}>
+    <input value={newNote} onChange={handleNoteChange} />
+    <button type="submit">save</button>
+  </form>
+);
+
+export { loginForm, noteForm };
