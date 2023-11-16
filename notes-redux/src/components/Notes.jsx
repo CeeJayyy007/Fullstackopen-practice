ec71539cb16f8aadcd55d9e79d62f66099479ec0
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-15 21:21:04 -0500

    refactor: use update service to toggle importance

--- notes-redux/src/components/Notes.jsx
+++ notes-redux/src/components/Notes.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
-import { toggleImportanceOf } from "../reducers/reducer";
+import { toggleImportanceOf } from "../reducers/noteReducer";
+import noteService from "../services/notes";
 
 const Note = ({ note, handleClick }) => {
   return (
@@ -12,7 +13,23 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = () => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
+
+  const notes = useSelector(({ filter, notes }) => {
+    if (filter === "ALL") {
+      return notes;
+    }
+    return filter === "IMPORTANT"
+      ? notes.filter((note) => note.important)
+      : notes.filter((note) => !note.important);
+  });
+
+  const handleToggleImportanceOf = async (note) => {
+    const id = note.id;
+    const changedNote = { ...note, important: !note.important };
+
+    const newNote = await noteService.update(id, changedNote);
+    dispatch(toggleImportanceOf(newNote));
+  };
 
   return (
     <ul>
@@ -20,7 +37,7 @@ const Notes = () => {
         <Note
           key={note.id}
           note={note}
-          handleClick={() => dispatch(toggleImportanceOf(note.id))}
+          handleClick={() => handleToggleImportanceOf(note)}
         />
       ))}
     </ul>
