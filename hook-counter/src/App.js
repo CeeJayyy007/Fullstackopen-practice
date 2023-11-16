ac29cd41a47308a5039ac8e653bef172c2bebdf0
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-16 09:47:46 -0500

    feat: add context api to app

--- hook-counter/src/App.js
+++ hook-counter/src/App.js
@@ -1,31 +1,44 @@
-import { useReducer } from 'react'
+import { useReducer } from "react";
+import CounterContext from "./CounterContext";
+import { useContext } from "react";
 
 const counterReducer = (state, action) => {
   switch (action.type) {
     case "INC":
-        return state + 1
+      return state + 1;
     case "DEC":
-        return state - 1
+      return state - 1;
     case "ZERO":
-        return 0
+      return 0;
     default:
-        return state
+      return state;
   }
-}
+};
+
+const Display = () => {
+  const [counter] = useContext(CounterContext);
+
+  return <div>{counter}</div>;
+};
+
+const Button = ({ type, label }) => {
+  const [counter, dispatch] = useContext(CounterContext);
+  return <button onClick={() => dispatch({ type })}>{label}</button>;
+};
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
+  const [counter, counterDispatch] = useReducer(counterReducer, 0);
 
   return (
-    <div>
-      <div>{counter}</div>
+    <CounterContext.Provider value={[counter, counterDispatch]}>
+      <Display counter={counter} />
       <div>
-        <button onClick={() => counterDispatch({ type: "INC"})}>+</button>
-        <button onClick={() => counterDispatch({ type: "DEC"})}>-</button>
-        <button onClick={() => counterDispatch({ type: "ZERO"})}>0</button>
+        <Button dispatch={counterDispatch} type="INC" label="+" />
+        <Button dispatch={counterDispatch} type="DEC" label="-" />
+        <Button dispatch={counterDispatch} type="ZERO" label="0" />
       </div>
-    </div>
-  )
-}
+    </CounterContext.Provider>
+  );
+};
 
-export default App
+export default App;
